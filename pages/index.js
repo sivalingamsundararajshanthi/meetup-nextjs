a2bc8cde1260c8d566eefd4facc109f00fa22e35
sivalingamsundararajshanthi <lingams200@gmail.com>
2023-06-19 22:05:32 -0400

    make page render evemts with styling

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import EventList from "../components/events/event-list";
+import { getFeaturedEvents } from "../dummy-data";
+
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+  return (
+    <div>
+      <ul>
+        <EventList items={featuredEvents} />
+      </ul>
+    </div>
+  );
+}
+
+export default HomePage;
